3298c41a1a91f2da1bee24115beaaf422ffffa84
ShihSyun Huang <shihsyun@gmail.com>
2019-02-12 14:06:49 +0800

    使用set方式做匹對

---
+++ Lesson02/odd_occurrences_in_array.py
@@ -0,0 +1,53 @@
+"""
+Task:OddOccurrencesInArray
+A non-empty array A consisting of N integers is given. The array contains an odd number of elements, and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+
+For example, in array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the elements at indexes 0 and 2 have value 9,
+the elements at indexes 1 and 3 have value 3,
+the elements at indexes 4 and 6 have value 9,
+the element at index 5 has value 7 and is unpaired.
+Write a function:
+
+def solution(A)
+
+that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+
+For example, given array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+the function should return 7, as explained in the example above.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an odd integer within the range [1..1,000,000];
+each element of array A is an integer within the range [1..1,000,000,000];
+all but one of the values in A occur an even number of times.
+Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+
+You can check it out the result at https://app.codility.com/demo/results/trainingWGY49S-XH9/ .
+"""
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+    # write your code in Python 3.6
+    # 依序scan array存入set，若set中無匹對值則存值，若有匹對值則移除set中存值，最後留下來的就是無匹對值。
+    tmp = set()
+    for index in A:
+        try:
+            tmp.remove(index)
+        except KeyError:
+            tmp.add(index)
+    
+    return tmp.pop()
+
+A = [9 ,3 ,9 ,3 ,9 ,7, 9]
+print(solution(A))
