3585c1312dbd511faf42895feb86af891200bd18
ShihSyun Huang <shihsyun@gmail.com>
2019-02-12 16:02:40 +0800

    利用array相加做出rotation

--- Lesson02/cyclic_rotation.py
+++ Lesson02/cyclic_rotation.py
@@ -39,5 +39,34 @@ In your solution, focus on correctness. The performance of your solution will no
 
 Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 
-You can check it out the result at .
+You can check it out the result at https://app.codility.com/demo/results/trainingBXQ39V-BTU/ .
 """
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A , K):
+    # write your code in Python 3.6
+    # 利用array相加做出rotation，A[-1]代表array的最後一個，依此類推。
+    # more detail please check it out at https://openhome.cc/Gossip/CodeData/PythonTutorial/NumericString.html .
+    if len(A) == 0:
+        return A
+
+    K =  K % len(A) 
+
+    return A[-K:] + A[:-K]
+
+# testcase 1 
+A = [3, 8, 9, 7, 6]
+K = 3
+print(solution(A , K))
+
+# testcase 2
+A = [0, 0, 0]
+K = 1
+print(solution(A , K))
+
+# testcase 3
+A = [1, 2, 3, 4]
+K = 4
+print(solution(A , K))
