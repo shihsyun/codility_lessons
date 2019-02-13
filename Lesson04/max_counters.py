07e8ff7f222709108b9cba1e739a69decf97062f
ShihSyun Huang <shihsyun@gmail.com>
2019-02-13 16:19:28 +0800

    照著題目規則跑迴圈，會遇到performance issue，只能拿到66%

---
+++ Lesson04/max_counters.py
@@ -0,0 +1,83 @@
+"""
+Task:MaxCounters
+Calculate the values of counters after applying all alternating operations: increase counter by 1; set value of all counters to current maximum.
+
+You are given N counters, initially set to 0, and you have two possible operations on them:
+
+increase(X) − counter X is increased by 1,
+max counter − all counters are set to the maximum value of any counter.
+A non-empty array A of M integers is given. This array represents consecutive operations:
+
+if A[K] = X, such that 1 ≤ X ≤ N, then operation K is increase(X),
+if A[K] = N + 1 then operation K is max counter.
+For example, given integer N = 5 and array A such that:
+
+    A[0] = 3
+    A[1] = 4
+    A[2] = 4
+    A[3] = 6
+    A[4] = 1
+    A[5] = 4
+    A[6] = 4
+the values of the counters after each consecutive operation will be:
+
+    (0, 0, 1, 0, 0)
+    (0, 0, 1, 1, 0)
+    (0, 0, 1, 2, 0)
+    (2, 2, 2, 2, 2)
+    (3, 2, 2, 2, 2)
+    (3, 2, 2, 3, 2)
+    (3, 2, 2, 4, 2)
+The goal is to calculate the value of every counter after all operations.
+
+Write a function:
+
+def solution(N, A)
+
+that, given an integer N and a non-empty array A consisting of M integers, returns a sequence of integers representing the values of the counters.
+
+Result array should be returned as an array of integers.
+
+For example, given:
+
+    A[0] = 3
+    A[1] = 4
+    A[2] = 4
+    A[3] = 6
+    A[4] = 1
+    A[5] = 4
+    A[6] = 4
+the function should return [3, 2, 2, 4, 2], as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N and M are integers within the range [1..100,000];
+each element of array A is an integer within the range [1..N + 1].
+Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+
+You can check it out the result at https://app.codility.com/demo/results/trainingSQTX4F-ZQZ/ .
+"""
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(N ,A):
+    
+    # write your code in Python 3.6
+    # 照著題目規則跑迴圈，會遇到performance issue，只能拿到66%
+
+    X = [0]*N
+    for idx in A:
+        if 1 <= idx <= N:
+            X[idx-1] += 1
+        else:
+            currentmax = max(X)
+            for i in range(0, N):
+                X[i] = currentmax
+
+    return X
+
+
+N = 5
+A = [3 ,4 ,4 ,6 ,1, 4, 4]
+print(solution(N , A))
