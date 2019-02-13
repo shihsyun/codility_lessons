dd6645dbb745f1ca779efa525096d55bae4761a3
ShihSyun Huang <shihsyun@gmail.com>
2019-02-13 19:49:23 +0800

    完成測驗

--- Lesson05/passing_cars.py
+++ Lesson05/passing_cars.py
@@ -41,7 +41,7 @@ N is an integer within the range [1..100,000];
 each element of array A is an integer that can have one of the following values: 0, 1.
 Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 
-You can check it out the result at  .
+You can check it out the result at https://app.codility.com/demo/results/trainingQR9HUV-P3Z/ .
 """
 
 # you can write to stdout for debugging purposes, e.g.
@@ -50,5 +50,26 @@ You can check it out the result at  .
 def solution(A):
     
     # write your code in Python 3.6
+    # 正規解法是將陣列翻轉後，開始搜尋陣列若遇west的車子則將數量遞增，若遇east則加總一次當前的west車子數量。
+    # 較容易理解的想法是先計算所有的west車子數量，再重頭搜尋陣列若遇east則加總一次當前的west，若遇到west則遞減。
+    # 演算法邏輯請參考 https://codesays.com/2014/solution-to-passing-cars-by-codility/#comment-502 
+    # 圖示請參考 https://drive.google.com/file/d/0B2GSXjlcPi0lT3otZ1N5ZXhoZlE/view 
+    # more detail can check it out at https://codesays.com/2014/solution-to-passing-cars-by-codility/#comment-502 .
+    # prefix sum 演算法內容可參考 https://rust-algo.club/sorting/counting_sort/index.html 
 
+    result = 0
+    west = A.count(1)
 
+    for car in A:
+        if car == 0:
+            result += west
+            if result > 1e9:
+                return -1
+        else:
+            west -= 1
+    
+    return result
+
+    
+A = [0, 1, 0, 1, 1]
+print(solution(A))
