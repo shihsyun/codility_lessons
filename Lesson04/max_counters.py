42b3a0802d251e5569d3b71fa8708c3268a6f3aa
ShihSyun Huang <shihsyun@gmail.com>
2019-02-13 17:50:02 +0800

    採用Lazy-write寫法改善Performance issue

--- Lesson04/max_counters.py
+++ Lesson04/max_counters.py
@@ -55,7 +55,7 @@ N and M are integers within the range [1..100,000];
 each element of array A is an integer within the range [1..N + 1].
 Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 
-You can check it out the result at https://app.codility.com/demo/results/trainingSQTX4F-ZQZ/ .
+You can check it out the result at https://app.codility.com/demo/results/training8XRQEK-GD6/ .
 """
 
 # you can write to stdout for debugging purposes, e.g.
@@ -64,18 +64,31 @@ You can check it out the result at https://app.codility.com/demo/results/trainin
 def solution(N ,A):
     
     # write your code in Python 3.6
-    # 照著題目規則跑迴圈，會遇到performance issue，只能拿到66%
+    # 使用lazy-write去處理performance issue，複雜度降為O(N + M)
+    # 需要兩個變數分別去紀錄
+    # 1. 截至目前為止須切齊的最大值，並在主迴圈與回傳檢查迴圈中做Ｘ的陣列元素替換
+    # 2. 計算在１<=X<=N情形下的操作最大值，並在>N的情形下，將須切齊的最大值替換成操作最大值
+    # more detial please check it out at https://codesays.com/2014/solution-to-max-counters-by-codility/#comment-445 .
 
-    X = [0]*N
+    result = [0]*N
+    max_to_set = 0
+    current_max = 0
     for idx in A:
         if 1 <= idx <= N:
-            X[idx-1] += 1
-        else:
-            currentmax = max(X)
-            for i in range(0, N):
-                X[i] = currentmax
+            
+            if result[idx-1] < max_to_set:
+                result[idx-1] = max_to_set
 
-    return X
+            result[idx-1] += 1
+
+            if current_max < result[idx-1]:
+                current_max = result[idx-1]
+
+        else:
+            max_to_set = current_max
+
+    result = [max(max_to_set, i) for i in result]
+    return result
 
 
 N = 5
