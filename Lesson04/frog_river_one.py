0c8fd75ef8c36fc883398d17a7950b47b8c95399
ShihSyun Huang <shihsyun@gmail.com>
2019-02-13 11:32:46 +0800

    使用Dict解題

---
+++ Lesson04/frog_river_one.py
@@ -0,0 +1,82 @@
+"""
+Task:FrogRiverOne
+Find the earliest time when a frog can jump to the other side of a river.
+
+A small frog wants to get to the other side of a river. The frog is initially located on one bank of the river (position 0) and wants to get to the opposite bank (position X+1). Leaves fall from a tree onto the surface of the river.
+
+You are given an array A consisting of N integers representing the falling leaves. A[K] represents the position where one leaf falls at time K, measured in seconds.
+
+The goal is to find the earliest time when the frog can jump to the other side of the river. The frog can cross only when leaves appear at every position across the river from 1 to X (that is, we want to find the earliest moment when all the positions from 1 to X are covered by leaves). You may assume that the speed of the current in the river is negligibly small, i.e. the leaves do not change their positions once they fall in the river.
+
+For example, you are given integer X = 5 and array A such that:
+
+  A[0] = 1
+  A[1] = 3
+  A[2] = 1
+  A[3] = 4
+  A[4] = 2
+  A[5] = 3
+  A[6] = 5
+  A[7] = 4
+In second 6, a leaf falls into position 5. This is the earliest time when leaves appear in every position across the river.
+
+Write a function:
+
+def solution(X, A)
+
+that, given a non-empty array A consisting of N integers and integer X, returns the earliest time when the frog can jump to the other side of the river.
+
+If the frog is never able to jump to the other side of the river, the function should return −1.
+
+For example, given X = 5 and array A such that:
+
+  A[0] = 1
+  A[1] = 3
+  A[2] = 1
+  A[3] = 4
+  A[4] = 2
+  A[5] = 3
+  A[6] = 5
+  A[7] = 4
+the function should return 6, as explained above.
+
+Write an efficient algorithm for the following assumptions:
+
+N and X are integers within the range [1..100,000];
+each element of array A is an integer within the range [1..X].
+Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+
+You can check it out the result at https://app.codility.com/demo/results/trainingCW2G6E-VQP/ .
+"""
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(X, A):
+    # write your code in Python 3.6
+    # 利用Dict紀錄每一步的最近時間，須注意若已出現過須排除，再回傳該Dict最大值。
+    pos = {}
+
+    for i in range(0, len(A)):
+        if A[i] not in pos:
+            pos.update({A[i] : i})
+
+    if len(pos) == X:
+        return max(pos.values())
+    
+    return -1
+
+# testcase 1
+X = 5
+A = [1 ,3 ,1 ,4 ,2 ,3, 5, 4]
+print(solution(X , A))
+
+# testcase 2
+X = 2
+A = [2 ,2 ,2 ,2]
+print(solution(X , A))
+
+# testcase 3
+X = 3
+A = [1, 3, 1, 3, 2, 1, 3]
+print(solution(X , A))
