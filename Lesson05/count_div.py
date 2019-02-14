f559a5707ab21876ba97e49d66b808c574df42d1
ShihSyun Huang <shihsyun@gmail.com>
2019-02-14 13:21:38 +0800

    改使用//運算後得到100%

--- Lesson05/count_div.py
+++ Lesson05/count_div.py
@@ -19,7 +19,7 @@ K is an integer within the range [1..2,000,000,000];
 A ≤ B.
 Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 
-You can check it out the result at https://app.codility.com/demo/results/training7K4WDG-JFW/ .
+You can check it out the result at https://app.codility.com/demo/results/training7FYMSS-MVT/ .
 """
 
 # you can write to stdout for debugging purposes, e.g.
@@ -27,15 +27,12 @@ You can check it out the result at https://app.codility.com/demo/results/trainin
 
 def solution(A ,B ,K):
     # write your code in Python 3.6
-    # Ａ或B-A能由K整除時須加1，拿到87%分數
-    # 錯誤訊息為 A = 101, B = 123M+, K = 10K got 12346 expected 12345
+    # 先計算A%K是否整除，若有須加1
+    # 之後分別將B與A除以K，並計算其差後回傳
+    # more detail please check it out at https://codesays.com/2014/solution-to-count-div-by-codility/#comment-1411 .
 
-    X = 0 if ((B - A) % K) == 0 else 1
-
-    if A % K == 0:
-        X = 1
-
-    return int((B - A) / K) + X
+    Ｘ = 1 if (A % K) == 0 else 0
+    return (B // K) - (A // K)  + X
 
 # testcase 1
 A = 6
