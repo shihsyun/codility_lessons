d04ba3877cf27fb18696a17f5c78fba5c73535d0
ShihSyun Huang <shihsyun@gmail.com>
2019-02-18 14:19:03 +0800

    參考網路作法後，完成測驗。

--- Lesson07/stonewall.py
+++ Lesson07/stonewall.py
@@ -26,7 +26,7 @@ Write an efficient algorithm for the following assumptions:
 N is an integer within the range [1..100,000];
 each element of array H is an integer within the range [1..1,000,000,000].
 Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
-You can check it out the result at https://app.codility.com/demo/results/trainingK5T72F-Z6D/ .
+You can check it out the result at https://app.codility.com/demo/results/training4H9T3Q-3JD/ .
 
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
@@ -36,27 +36,28 @@ You can check it out the result at https://app.codility.com/demo/results/trainin
 def solution(H):
 
     # write your code in Python 3.6
-    # 將Ｈ各元素值存入並與前一個元素做比對，如果小於則將count+1
+    # 只有當先前高度大於目前高度時需要使用新的磚塊，所以使用一個stack紀錄高度
+    # 使用stack的原因是因為相同的高度下，前面順序的高度可以當底，所以先從後面順序提取紀錄的高度
+    # 若stack為空或當前一個高度與目前高度相同時，則不做任何事，否則都需要磚塊並紀錄高度
+    # 官方解答 https://codility.com/media/train/solution-stone-wall.pdf
+    # more detail please check it out at https://www.martinkysel.com/codility-stonewall-solution/ .
 
-    wall = [H[0]]
+    blocks = 0
+    stack = []
 
-    for idx in range(len(H)):
+    for X in H:
 
-        if H[idx] == wall[-1]:
+        while len(stack) != 0 and stack[-1] > X:
+            stack.pop()
+                
+        if len(stack) != 0 and stack[-1] == X:
             continue
-        else:
-            wall.append(H[idx])
 
-    if len(wall) == 1: return 1
+        stack.append(X)
+        blocks += 1
+    
+    return blocks
 
-    count = 0
-    prev = wall[0]
-    for elem in wall:
-        if prev != elem:
-            count += 1
-        prev = elem
- 
-    return count
 
 #testcase 1
 H = [8, 8, 5, 7, 9, 8, 7 ,4, 8]
@@ -66,6 +67,10 @@ print(solution(H))
 H = [1, 1, 1]
 print(solution(H))
 
-#testcase 3
+#testcase ４
 H = [3, 2, 1]
+print(solution(H))
+
+#testcase ４
+H = [2, 3, 2, 1]
 print(solution(H))
