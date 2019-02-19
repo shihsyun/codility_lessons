c02d23fac177d11251331b7eae6a56b96a62740e
ShihSyun Huang <shihsyun@gmail.com>
2019-02-19 13:46:03 +0800

    按照題目定義使用迴圈計算，複雜度為O(N**3)，拿到53%。

---
+++ Lesson09/max_double_slice_sum.py
@@ -0,0 +1,81 @@
+"""
+Task:MaxDoubleSliceSum
+Find the maximal sum of any double slice.
+
+A non-empty array A consisting of N integers is given.
+
+A triplet (X, Y, Z), such that 0 ≤ X < Y < Z < N, is called a double slice.
+
+The sum of double slice (X, Y, Z) is the total of A[X + 1] + A[X + 2] + ... + A[Y − 1] + A[Y + 1] + A[Y + 2] + ... + A[Z − 1].
+
+For example, array A such that:
+
+    A[0] = 3
+    A[1] = 2
+    A[2] = 6
+    A[3] = -1
+    A[4] = 4
+    A[5] = 5
+    A[6] = -1
+    A[7] = 2
+contains the following example double slices:
+
+double slice (0, 3, 6), sum is 2 + 6 + 4 + 5 = 17,
+double slice (0, 3, 7), sum is 2 + 6 + 4 + 5 − 1 = 16,
+double slice (3, 4, 5), sum is 0.
+The goal is to find the maximal sum of any double slice.
+
+Write a function:
+
+def solution(A)
+
+that, given a non-empty array A consisting of N integers, returns the maximal sum of any double slice.
+
+For example, given:
+
+    A[0] = 3
+    A[1] = 2
+    A[2] = 6
+    A[3] = -1
+    A[4] = 4
+    A[5] = 5
+    A[6] = -1
+    A[7] = 2
+the function should return 17, because no double slice of array A has a sum of greater than 17.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [3..100,000];
+each element of array A is an integer within the range [−10,000..10,000].
+Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+
+You can check it out the result at https://app.codility.com/demo/results/trainingNTBNDA-FDG/ .
+
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+"""
+
+def solution(A):
+
+        # write your code in Python 3.6
+        # 按照題目定義使用迴圈計算，複雜度為O(N**3)，拿到53%。
+
+    total = 0
+
+    for idx in range(len(A)):
+        for i in range(idx, len(A)):
+            for j in range(i, len(A)):
+                if (idx < i) and (i < j):
+                    total  = max(total, sum(A[idx:j+1])-A[idx]-A[i]-A[j])
+
+    return total
+
+
+#testcase 1
+A = [3, 2, 6, -1, 4, 5, -1, 2]
+print(solution(A))
+
+#testcase 2
+A = [5, 17, 0, 3]
+print(solution(A))
