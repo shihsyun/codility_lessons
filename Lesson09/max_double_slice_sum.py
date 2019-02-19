8b704bdd2be4624c1721094d6d072c708b294e0e
ShihSyun Huang <shihsyun@gmail.com>
2019-02-19 15:08:35 +0800

    參考網路作法後，達成100%。

--- Lesson09/max_double_slice_sum.py
+++ Lesson09/max_double_slice_sum.py
@@ -49,7 +49,7 @@ N is an integer within the range [3..100,000];
 each element of array A is an integer within the range [−10,000..10,000].
 Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 
-You can check it out the result at https://app.codility.com/demo/results/trainingNTBNDA-FDG/ .
+You can check it out the result at https://app.codility.com/demo/results/training89782B-Y85/ .
 
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
@@ -58,19 +58,29 @@ You can check it out the result at https://app.codility.com/demo/results/trainin
 
 def solution(A):
 
-        # write your code in Python 3.6
-        # 按照題目定義使用迴圈計算，複雜度為O(N**3)，拿到53%。
+    # write your code in Python 3.6
+    # 將原始陣列分成左右兩個陣列，左陣列為依序計算A[1]開始至最後的元素數值總和
+    # 右陣列為反向自A[-2]開始計算至最後，以上皆若遇負值需填0。
+    # 最後再將左右兩陣列數值加總取最大值後回傳
+    # https://en.wikipedia.org/wiki/Maximum_subarray_problem
+    # more detail please check it out at https://www.martinkysel.com/codility-maxdoubleslicesum-solution/ .
 
-    total = 0
+    size = len(A)
+    L_slice = [0]*size
+    R_slice = [0]*size
 
-    for idx in range(len(A)):
-        for i in range(idx, len(A)):
-            for j in range(i, len(A)):
-                if (idx < i) and (i < j):
-                    total  = max(total, sum(A[idx:j+1])-A[idx]-A[i]-A[j])
+    for idx in range(1, size):
+        L_slice[idx] = max(0, L_slice[idx-1] + A[idx])
 
-    return total
+    for idx in reversed(range(size-1)):
+        R_slice[idx] = max(0, A[idx] + R_slice[idx+1])
 
+    once_total = L_slice[0] + R_slice[2]
+
+    for idx in range(1, size-1):
+        once_total = max(once_total, L_slice[idx-1] + R_slice[idx+1])
+
+    return once_total
 
 #testcase 1
 A = [3, 2, 6, -1, 4, 5, -1, 2]
