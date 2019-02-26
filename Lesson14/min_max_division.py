0bfc15b9dc1a338f11a6da93f7c0637e45efe17a
ShihSyun Huang <shihsyun@gmail.com>
2019-02-26 09:42:22 +0800

    查詢網路作法後，完成測驗，複雜度為O(N*log(N+M))。

--- Lesson14/min_max_division.py
+++ Lesson14/min_max_division.py
@@ -51,30 +51,55 @@ M is an integer within the range [0..10,000];
 each element of array A is an integer within the range [0..M].
 Copyright 2009–2019 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 
-You can check it out the result at https://app.codility.com/demo/results/trainingQN25HP-5QT/ .
+You can check it out the result at https://app.codility.com/demo/results/training2EPFEU-EGJ/ .
 
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
 """
 
-def solution(K, M, A):
     # write your code in Python 3.6
-    # 按照題目要求，使用迴圈求最大值。
+    # 查詢網路作法後，得知如果K==1時，large sum就是sum(A)，K>=len(A)時，即代表large sum是max(A)。
+    # 即可依據這兩個邊界條件進行二分搜尋，找到可能的block size sum，再依據這個可能值依序掃描陣列檢驗是否能分成K個block。
+    # more detail please check it out at https://www.martinkysel.com/codility-minmaxdivision-solution/ .
 
-    result = [0]*K
-    d = len(A) // K
+def check(A, K, max_block_sum):
+    block_sum = 0
+    count = 0
 
-    if d == 0: return max(A)
+    for elem in A:
+        if block_sum + elem > max_block_sum:
+            block_sum = elem
+            count += 1
+        else:
+            block_sum += elem
+        
+        if count >= K:
+            return False
 
-    for idx in range(K):
-        result[idx] = A[idx*d:idx*d+d]
+    return True
 
-    total = 0
-    for elem in result:
-        total = max(total, sum(elem))
 
-    return total
+def solution(K, M, A):
+
+    lower_bound = max(A)
+    upper_bound = sum(A)
+
+    if K == 1:
+        return upper_bound
+
+    if K >= len(A):
+        return lower_bound
+
+    while lower_bound <= upper_bound:
+        possible_candidate = (lower_bound + upper_bound) // 2
+
+        if check(A, K, possible_candidate):
+            upper_bound = possible_candidate - 1
+        else:
+            lower_bound = possible_candidate + 1
+
+    return lower_bound
 
 # testcase 1
 K = 3
